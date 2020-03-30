5c1f2e4cd7e8ff1c4bd4f17d9071608e8c6392a6
Ken Tsen <ken@9dimensions.io>
2020-03-30 14:08:00 +0800

    Apple Login.

---
+++ Assets/Scripts/AppleLogin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SignInWithApple;
+using UnityEngine.UI;
+
+public class AppleLogin : MonoBehaviour
+{
+	public Button btnAppleLogin;
+
+	private void Start()
+	{
+		btnAppleLogin.onClick.AddListener(BtnLoginEvent);
+		gameObject.AddComponent<SignInWithApple>();
+	}
+
+	private void BtnLoginEvent()
+	{
+		SignInWithApple signIn = GetComponent<SignInWithApple>();
+		signIn.Login(Callback);
+	}
+
+	private void Callback(SignInWithApple.CallbackArgs args)
+	{
+		Debug.Log("This is your id:" + args.userInfo.userId);
+	}
+}
